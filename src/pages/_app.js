62852601bc943e08a63293ece3b6c35d44b74277
Ayush <mhetreayush1719@gmail.com>
2023-03-23 16:46:26 +0530

    Verification

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,16 @@
-import '@/styles/globals.css'
+import "@/styles/globals.css";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <meta
+          name="google-site-verification"
+          content="LAFLPJO73XDxXz1ZA7Oa8g_g3xdfcL24i9sEhlt9G6o"
+        />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
